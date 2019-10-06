a86a5e05e14b781b6f823618501fd9eaee612342
taranek <32940881+taranek@users.noreply.github.com>
2019-10-06 21:55:31 +0200

    Added database-seeding migration on DbContext

---
+++ ParkingApp.Data/Migrations/20191006195049_SeedingMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingApp.Data.Migrations
+{
+    public partial class SeedingMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Spots",
+                columns: new[] { "Id", "Code", "Company", "Level", "ParkingLot", "PrimaryOwner" },
+                values: new object[] { 1, "XX", "Company", -1, "EP1", "Jan Kowalski" });
+
+            migrationBuilder.InsertData(
+                table: "Spots",
+                columns: new[] { "Id", "Code", "Company", "Level", "ParkingLot", "PrimaryOwner" },
+                values: new object[] { 2, "XY", "Company2", -1, "EP2", "Jose Alcara" });
+
+            migrationBuilder.InsertData(
+                table: "Spots",
+                columns: new[] { "Id", "Code", "Company", "Level", "ParkingLot", "PrimaryOwner" },
+                values: new object[] { 3, "ABC", "Company1", 1, "EP1", "Jan Nowak" });
+
+            migrationBuilder.InsertData(
+                table: "Bookings",
+                columns: new[] { "Id", "DateEnd", "DateStart", "Owner", "SpotId" },
+                values: new object[,]
+                {
+                    { 3, new DateTime(2019, 10, 4, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8272), new DateTime(2019, 10, 2, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8268), "Julie Lerman", 1 },
+                    { 4, new DateTime(2019, 10, 10, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8280), new DateTime(2019, 10, 8, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8276), "Julie Lerman", 1 },
+                    { 2, new DateTime(2019, 10, 9, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8261), new DateTime(2019, 10, 7, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(8245), "Kyle SImpson", 2 },
+                    { 1, new DateTime(2019, 10, 7, 21, 50, 48, 829, DateTimeKind.Local).AddTicks(7017), new DateTime(2019, 10, 5, 21, 50, 48, 827, DateTimeKind.Local).AddTicks(4402), "Deborah Kurata", 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Spots",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Spots",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Spots",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- ParkingApp.Data/ParkingContext.cs
+++ ParkingApp.Data/ParkingContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using ParkingApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace ParkingApp.Data
 {
@@ -30,14 +32,87 @@ namespace ParkingApp.Data
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder
-                .UseSqlServer("Server=TOMEK;Database=ParkingAppData;Trusted_Connection=True;")
+                .UseSqlServer("Server=TOMEK;Database=ParkingAppDataSeeded;Trusted_Connection=True;")
                 .UseLoggerFactory(DbLogger);
             }
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Spot>().HasMany(s => s.SpotBookings).WithOne(b => b.BookedSpot).HasForeignKey(b => b.SpotId);
+            modelBuilder.Seed();
+        }
+        
+    }
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Spot>().HasData(
+                new Spot
+                {
+                    Id = 1,
+                    Code = "XX",
+                    Company = "Company",
+                    Level = -1,
+                    ParkingLot = "EP1",
+                    PrimaryOwner = "Jan Kowalski",
+                    SpotBookings = new List<Booking>(),
+                },
+                new Spot
+                {
+                    Id = 2,
+                    Code = "XY",
+                    Company = "Company2",
+                    Level = -1,
+                    ParkingLot = "EP2",
+                    PrimaryOwner = "Jose Alcara",
+                    SpotBookings = new List<Booking>(),
+                },
+                new Spot
+                {
+                    Id = 3,
+                    Code = "ABC",
+                    Company = "Company1",
+                    Level = 1,
+                    ParkingLot = "EP1",
+                    PrimaryOwner = "Jan Nowak",
+                    SpotBookings = new List<Booking>(),
+                });
+            modelBuilder.Entity<Booking>().HasData(
+                new Booking
+                {
+                    Id = 1,
+                    SpotId = 3,
+                    DateStart = DateTime.Now.AddDays(-1),
+                    DateEnd = DateTime.Now.AddDays(1),
+                    Owner = "Deborah Kurata",
+                },
+                 new Booking
+                 {
+                     Id = 2,
+                     SpotId = 2,
+                     DateStart = DateTime.Now.AddDays(1),
+                     DateEnd = DateTime.Now.AddDays(3),
+                     Owner = "Kyle SImpson",
+                 },
+                 new Booking
+                 {
+                     Id = 3,
+                     SpotId = 1,
+                     DateStart = DateTime.Now.AddDays(-4),
+                     DateEnd = DateTime.Now.AddDays(-2),
+                     Owner = "Julie Lerman",
+                 },
+                 new Booking
+                 {
+                     Id = 4,
+                     SpotId = 1,
+                     DateStart = DateTime.Now.AddDays(2),
+                     DateEnd = DateTime.Now.AddDays(4),
+                     Owner = "Julie Lerman",
+                 }
+            );
         }
     }
-}
+    }
 
